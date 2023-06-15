225f843f33113e948e5772674de6f37a549aabba
ANIEBIERE <justinaumoh2@gmail.com>
2023-06-15 12:14:22 -0700

    5-to_integer.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,13 +1,3 @@
 #!/usr/bin/node
-// using the isNaN() function to check if the first argument (arg) can be converted to an integer.
-
-const arg = process.argv[2];
-
-// using the ! (logical NOT) operator to invert the boolean value returned by isNaN()
-// printing the result using template literals to interpolate the value into the output string in the specified format.
-if (!isNaN(parseInt(arg))) {
-  console.log(`My number: ${parseInt(arg)}`);
-// If it can't be converted to an integer, we print "Not a number" instead.
-} else {
-  console.log('Not a number');
-}
+const num = Math.floor(Number(process.argv[2]));
+console.log(isNaN(num) ? 'Not a number' : `My number: ${num}`);
